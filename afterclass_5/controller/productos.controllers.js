d334374469a5cc5f45544e85c9578c2a37ae5a55
santiago <santiagocendra.sc@gmail.com>
2022-07-06 00:19:17 -0300

    ContenedorMySQL

--- afterclass_5/controller/productos.controllers.js
+++ afterclass_5/controller/productos.controllers.js
@@ -1,16 +1,18 @@
-const ContenedorArchivo = require("../contenedores/ContenedorArchivo");
+const { options } = require("../db/mysql");
 
+const ContenedorArchivo = require("../contenedores/ContenedorArchivo");
+const ContenedorMySQL = require("../contenedores/ContenedorMySQL");
 const contenedorArchivoProductos = new ContenedorArchivo("./products.json");
+const contenedorMySQLProductos = new ContenedorMySQL(options, "products");
 
 //GET
 const getProducts = async (req, res, next) => {
- 
-  return  contenedorArchivoProductos.findAll()
-  .then(products => {
-    console.log(products)
-    
+  return contenedorMySQLProductos.findAll()
+  .then((products) => {
+    console.log(products);
+
     return res.json(products);
-  })
+  });
 };
 
 //GET POR ID
@@ -19,12 +21,11 @@ const getProductForId = async (req, res, next) => {};
 //POST PRODUCT
 const saveProduct = async (req, res, next) => {
   const data = req.body;
-  
-  return contenedorArchivoProductos.create(data)
-    .then(newProduct => {
-      console.log(newProduct);
-      return res.status(201).json(newProduct);
-    })
+
+  return contenedorMySQLProductos.create(data).then((newProduct) => {
+    console.log(newProduct);
+    return res.status(201).json(newProduct);
+  });
 };
 
 //Actualiza producto por su id
