7c705cff2303836bf15b85e794cf83499a1b468f
santiago <santiagocendra.sc@gmail.com>
2022-07-10 02:21:38 -0300

    2da entrega

--- afterclass_5/controller/productos.controllers.js
+++ afterclass_5/controller/productos.controllers.js
@@ -1,19 +1,20 @@
 const { options } = require("../db/mysql");
-// const storage = require('../daos')
-// const productsStorage = storage().products
+
+const storage = require('../daos') //hacemos referencia al index de daos. 
+const productsStorage = storage().products //accedemos al objeto products. dentro del switch de index de daos. 
 
 // const ContenedorArchivo = require("../contenedores/ContenedorArchivo");
 // const ContenedorMySQL = require("../contenedores/ContenedorMySQL");
 
-const ProductosDAOArchivo = require("../daos/productos/ProductosDaoArchivo");
-const productosDAO = new ProductosDAOArchivo();
+// const ProductosDAOArchivo = require("../daos/productos/ProductosDaoArchivo");
+// const productosDAO = new ProductosDAOArchivo();
 
 // const contenedorArchivoProductos = new ContenedorArchivo("./products.json");
 // const contenedorMySQLProductos = new ContenedorMySQL(options, "products");
 
 //GET
 const getProducts = async (req, res, next) => {
-  return productosDAO.findAll().then((products) => {
+  return productsStorage.findAll().then((products) => {
     console.log(products);
 
     return res.json(products);
@@ -27,7 +28,7 @@ const getProductForId = async (req, res, next) => {};
 const saveProduct = async (req, res, next) => {
   const data = req.body;
 
-  return productosDAO.create(data).then((newProduct) => {
+  return productsStorage.create(data).then((newProduct) => {
     console.log(newProduct);
     return res.status(201).json(newProduct);
   });
