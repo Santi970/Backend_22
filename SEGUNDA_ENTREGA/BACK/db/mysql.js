b311b2a20a22855e26549112c259c021a79b833c
santiago <santiagocendra.sc@gmail.com>
2022-07-13 00:09:57 -0300

    22

--- SEGUNDA_ENTREGA/BACK/db/mysql.js
+++ SEGUNDA_ENTREGA/BACK/db/mysql.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   client: "mysql",
-//   connection: {
-//     host: process.env.DB_HOST,
-//     port: 3306,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: "ecommerce",
-//   },
-//   pool: { min: 0, max: 10 },
-// };
 
 const options = {
   client: "mysql",
